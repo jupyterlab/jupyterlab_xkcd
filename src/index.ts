971af40b48fe529e73de21c521296e6abbf2f89d
Steven Silvester <steven.silvester@ieee.org>
2018-08-16 06:03:49 -0500

    Show a comic in the panel

--- src/index.ts
+++ src/index.ts
@@ -29,6 +29,19 @@ const extension: JupyterLabPlugin<void> = {
     widget.title.label = 'xkcd.com';
     widget.title.closable = true;
 
+    // Add an image element to the panel
+    let img = document.createElement('img');
+    widget.node.appendChild(img);
+
+    // Fetch info about a random comic
+    fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+      return response.json();
+    }).then(data => {
+      img.src = data.img;
+      img.alt = data.title;
+      img.title = data.alt;
+    });
+
     // Add an application command
     const command: string = 'xkcd:open';
     app.commands.addCommand(command, {
