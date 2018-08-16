281e01e3a4d6b54549a453546add64fceaac072e
Steven Silvester <steven.silvester@ieee.org>
2018-08-16 06:06:54 -0500

    Refactor, refresh comic

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,92 @@ import {
 } from '@jupyterlab/apputils';
 
 import {
+  Message
+} from '@phosphor/messaging';
+
+import {
   Widget
 } from '@phosphor/widgets';
 
 import '../style/index.css';
+
+
+/**
+ * An xckd comic viewer.
+ */
+class XkcdWidget extends Widget {
+  /**
+   * Construct a new xkcd widget.
+   */
+  constructor() {
+    super();
+
+    this.id = 'xkcd-jupyterlab';
+    this.title.label = 'xkcd.com';
+    this.title.closable = true;
+    this.addClass('jp-xkcdWidget');
+
+    this.img = document.createElement('img');
+    this.img.className = 'jp-xkcdCartoon';
+    this.node.appendChild(this.img);
+
+    this.img.insertAdjacentHTML('afterend',
+      `<div class="jp-xkcdAttribution">
+        <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+          <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+        </a>
+      </div>`
+    );
+  }
+
+  /**
+   * The image element associated with the widget.
+   */
+  readonly img: HTMLImageElement;
+
+  /**
+   * Handle update requests for the widget.
+   */
+  onUpdateRequest(msg: Message): void {
+    fetch('https://egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+      return response.json();
+    }).then(data => {
+      this.img.src = data.img;
+      this.img.alt = data.title;
+      this.img.title = data.alt;
+    });
+  }
+};
+
+
+/**
+ * Activate the xckd widget extension.
+ */
+function activate(app: JupyterFrontEnd, palette: ICommandPalette) {
+  console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+
+  // Create a single widget
+  let widget: XkcdWidget = new XkcdWidget();
+
+  // Add an application command
+  const command: string = 'xkcd:open';
+  app.commands.addCommand(command, {
+    label: 'Random xkcd comic',
+    execute: () => {
+      if (!widget.isAttached) {
+        // Attach the widget to the main work area if it's not there
+        app.shell.add(widget, 'main');
+      }
+      // Refresh the comic in the widget
+      widget.update();
+      // Activate the widget
+      app.shell.activateById(widget.id);
+    }
+  });
+
+  // Add the command to the palette.
+  palette.addItem({ command, category: 'Tutorial' });
+};
 
 
 /**
@@ -20,56 +102,7 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab_xkcd',
   autoStart: true,
   requires: [ICommandPalette],
-  activate: (app: JupyterFrontEnd, palette: ICommandPalette) => {
-    console.log('JupyterLab extension jupyterlab_xkcd is activated!');
-
-    // Create a single widget
-    let widget: Widget = new Widget();
-    widget.id = 'xkcd-jupyterlab';
-    widget.title.label = 'xkcd.com';
-    widget.title.closable = true;
-    widget.addClass('jp-xkcdWidget'); // new line
-
-    // Add an image element to the panel
-    let img = document.createElement('img');
-    img.className = 'jp-xkcdCartoon'; // new line
-    widget.node.appendChild(img);
-
-    // New: add an attribution badge
-    img.insertAdjacentHTML('afterend',
-      `<div class="jp-xkcdAttribution">
-        <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
-          <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
-        </a>
-      </div>`
-    );
-
-    // Fetch info about a random comic
-    fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
-      return response.json();
-    }).then(data => {
-      img.src = data.img;
-      img.alt = data.title;
-      img.title = data.alt;
-    });
-
-    // Add an application command
-    const command: string = 'xkcd:open';
-    app.commands.addCommand(command, {
-      label: 'Random xkcd comic',
-      execute: () => {
-        if (!widget.isAttached) {
-          // Attach the widget to the main work area if it's not there
-          app.shell.addToMainArea(widget);
-        }
-        // Activate the widget
-        app.shell.activateById(widget.id);
-      }
-    });
-
-    // Add the command to the palette.
-    palette.addItem({command, category: 'Tutorial'});
-  }
+  activate: activate
 };
 
 export default extension;
